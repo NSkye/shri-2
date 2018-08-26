086ac93cbb20de3d312d3be0e82491c8160e64e5
Nikolai <nikolai.laevskii@yandex.ru>
2018-08-26 07:51:16 +0300

    Более умный футер, автопрефиксы

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
                 use: [
                     { loader: 'style-loader' },
                     { loader: 'css-loader' },
+                    { loader: 'postcss-loader' },
                     { loader: 'stylus-loader' }
                 ]
             },
@@ -23,7 +24,8 @@ module.exports = {
                 test: /\.css$/,
                 use: [
                     { loader: 'style-loader' },
-                    { loader: 'css-loader' }
+                    { loader: 'css-loader' },
+                    { loader: 'postcss-loader' }
                 ]
             },
             {

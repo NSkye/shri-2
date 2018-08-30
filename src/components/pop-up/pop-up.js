9248647c2a34a1fa427dcd2d6b9f6cef8f8ddb82
Nikolai <nikolai.laevskii@yandex.ru>
2018-08-30 10:20:47 +0300

    Исправлен баг с позиционированием попапа на мобильных телефонах - исправлен баг с позиционированием попапа на телефонах, теперь всегда по центру - добавлена функция для определения данных устройства, чтоб потом вывести их в попап

--- src/components/pop-up/pop-up.js
+++ src/components/pop-up/pop-up.js
@@ -5,8 +5,21 @@ const global_API = require('../global_API');
 
 const popup = document.querySelector('.pop-up');
 const popupContainerElement = document.querySelector('.pop-up-container');
+const body = document.querySelector('.body');
+
+function getContainerInfo(container) {
+    const infoBlock = [...container.children].find(child => ~[...child.classList].indexOf('device__info'))
+    if (!infoBlock) return {};
+    return {
+        name: [...infoBlock.children].find(child => ~[...child.classList].indexOf('device__name')).childNodes[0].data,
+        status: [...infoBlock.children].find(child => ~[...child.classList].indexOf('device__status')).childNodes[0].data,
+        popupType: container.attributes.popuptype.nodeValue
+    }
+}
 
 function open(container) {
+    console.log(getContainerInfo(container));
+
     const viewportOffset = container.getBoundingClientRect();
     const [
         initialHeight,
@@ -20,16 +33,18 @@ function open(container) {
         viewportOffset.top
     ]
 
+    const [ h, w ] = [ 200, 250 ];
+
     popup.style.height = initialHeight + 'px';
     popup.style.width = initialWidth + 'px';
     popup.style.marginTop = initialPositionY + 'px';
     popup.style.marginLeft = initialPositionX + 'px';
 
     popupContainer.open();
-    popup.style.height = '400px';
-    popup.style.width = '500px';
-    popup.style.marginTop = window.innerHeight/2 - 400/2 + 'px';
-    popup.style.marginLeft = window.innerWidth/2 - 500/2 + 'px';
+    popup.style.height = h + 'px';
+    popup.style.width = w + 'px';
+    popup.style.marginTop = body.clientHeight/2 - h/2 + 'px';
+    popup.style.marginLeft = body.clientWidth/2 - w/2 + 'px';
     popupContainer.darkenAfter(0);
 
     global_API.add('CURRENT_POPUP', { initialHeight, initialWidth, initialPositionX, initialPositionY });
